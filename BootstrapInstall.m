3dbf4c7bc6a30bb761353e5393b5eab87cd1e787
jkuczm <Jakub.Kuczmarski@gmail.com>
2015-03-30 16:48:58 +0200

    Version bump: 0.1.1

--- BootstrapInstall.m
+++ BootstrapInstall.m
@@ -5,7 +5,7 @@ Get["https://raw.githubusercontent.com/jkuczm/MathematicaBootstrapInstaller/v0.1
 
 BootstrapInstall[
 	"CellsToTeX",
-	"https://github.com/jkuczm/MathematicaCellsToTeX/releases/download/v0.1.0/CellsToTeX.zip"
+	"https://github.com/jkuczm/MathematicaCellsToTeX/releases/download/v0.1.1/CellsToTeX.zip"
 	,
 	{{
 		"SyntaxAnnotations",
--- PacletInfo.m
+++ PacletInfo.m
@@ -1,10 +1,10 @@
 (* Paclet Info File *)
 
-(* created 2015/03/27*)
+(* created 2015/03/30*)
 
 Paclet[
     Name -> "CellsToTeX",
-    Version -> "0.1.0",
+    Version -> "0.1.1",
     MathematicaVersion -> "6+",
     Description -> "Convert Mathematica cells to TeX, retaining formatting",
     Creator -> "Jakub Kuczmarski",
