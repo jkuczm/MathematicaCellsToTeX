53366595b8cd71a7a650b364fb5f536a246ffe79
jkuczm <Jakub.Kuczmarski@gmail.com>
2016-01-30 16:45:11 +0100

    Version bump: 0.1.4

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,10 +1,10 @@
 (* Paclet Info File *)
 
-(* created 2015/11/03*)
+(* created 2016/01/30*)
 
 Paclet[
     Name -> "CellsToTeX",
-    Version -> "0.1.3",
+    Version -> "0.1.4",
     MathematicaVersion -> "6+",
     Description -> "Convert Mathematica cells to TeX, retaining formatting",
     Creator -> "Jakub Kuczmarski",
