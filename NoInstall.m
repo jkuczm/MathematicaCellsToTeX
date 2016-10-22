ab5d8613b51b642ec005e44bff4adcfac4873a0b
jkuczm <Jakub.Kuczmarski@gmail.com>
2016-10-22 12:10:44 +0200

    Use SyntaxAnnotations v0.2.1

--- NoInstall.m
+++ NoInstall.m
@@ -1,6 +1,6 @@
 (* ::Package:: *)
 
 Block[{$ContextPath},
-	Import["https://raw.githubusercontent.com/jkuczm/MathematicaSyntaxAnnotations/v0.2.0/SyntaxAnnotations/SyntaxAnnotations.m"];
+	Import["https://raw.githubusercontent.com/jkuczm/MathematicaSyntaxAnnotations/v0.2.1/SyntaxAnnotations/SyntaxAnnotations.m"];
 ]
 Import["https://raw.githubusercontent.com/jkuczm/MathematicaCellsToTeX/master/CellsToTeX/CellsToTeX.m"]
